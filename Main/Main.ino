ba287167935b82a30c98f9bd8de7be614319b71a
Amaf14 <alexmaf14@yahoo.com>
2016-05-11 10:01:07 +0300

    Update Main.ino

--- Main/Main.ino
+++ Main/Main.ino
@@ -9,13 +9,13 @@ int simbol[10];
 
 void asfisare() {
 display.setcursor(10, 10);
-display.drawRect(10, 10, 10, 10, BLACK);
-display.drawRect(20, 10, 10, 10, BLACK);
-display.drawRect(30, 10, 10, 10, BLACK);
-
-
-
-
+display.drawRect(10, 10, 7, 10, BLACK);
+display.drawRect(20, 10, 7, 10, BLACK);
+display.drawRect(30, 10, 7, 10, BLACK);
+display.setcursor(40, 30);
+display.print( F("Spin it!"));
+display.display();
+}
 
 
 
